685701ac085a79988f5be38acf6c864dfbbf1bd6
Technocrazy8 <jevan2001@gmail.com>
2022-04-26 20:20:40 -0700

    modded scene

--- Scene.java
+++ Scene.java
@@ -1,3 +1,14 @@
 public class Scene {
-    
+    String title;
+    String description;
+    int budget;
+    Roles[] parts;
+    int shotcounter;
+    public Scene(String title,String description,int budgets, Roles[] parts,int shotcounter){
+        this.title=title;
+        this.description=description;
+        this.budget=budgets;
+        this.parts=parts;
+        this.shotcounter = shotcounter;
+    }
 }
