27dc135dd54dce82cfcd4ecf37d1dbef6f15b5f6
Technocrazy8 <jevan2001@gmail.com>
2022-04-26 20:01:06 -0700

    added constructor and fields to player

--- Player.java
+++ Player.java
@@ -1,3 +1,16 @@
 public class Player {
-    
+    int rank;
+    int credits;
+    int money;
+    String name;
+    int id;
+    int chips;
+    public Player(String name, int id){
+        this.rank=0;
+        this.credits=0;
+        this.money=0;
+        this.id = id;
+        this.name=name;
+        this.chips = 0;
+    }
 }
