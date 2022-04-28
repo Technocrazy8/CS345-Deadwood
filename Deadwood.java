012e5e7f1f110cd0c4d11f82fb50923eb67598bd
Teariest <clementfaisandier@outlook.com>
2022-04-27 18:34:58 -0700

    Refining

--- Deadwood.java
+++ Deadwood.java
@@ -28,12 +28,24 @@ public class Deadwood {
 
     }
 
+    public void resetBoard(Board board) {
+
+    }
+
+    public Scene[] shuffleScenes(Scene[]) {
+
+    }
+
     public void move(Player player, Set set){
 
     }
 
     public void takeRole(Player player, Role role) {
-        
+
+    }
+
+    public void upgrade(Player player) {
+
     }
 
     public void act(Player player, Set set, Scene scene){
@@ -43,4 +55,17 @@ public class Deadwood {
     public void rehearse(Player player,Set set,Scene scene){
 
     }
+
+    public void calculateScores(Board board) {
+
+    }
 }
+/**
+ * Questions:
+ * - How should we keep track of each player's position?
+ *      Possible Solutions:
+ *      - Board keeps track of where each player is
+ *      - Role keeps track of player and then each step in the hierarchy goes fetch that
+ *      - Sets and scenes keep track of it
+ *      - Any kind of combinations of this.
+ */
