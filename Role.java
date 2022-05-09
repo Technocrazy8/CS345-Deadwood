07c2e8abec863b3962df6a1290d6c0499aff9415
Technocrazy8 <jevan2001@gmail.com>
2022-05-09 15:54:56 -0700

    modded parse and role

--- Role.java
+++ Role.java
@@ -9,4 +9,19 @@ public class Role{
     int rehearseBonus; // may be trivial
     int actor; // player id
 
+    public void setTitle(String t){
+        this.title =t;
+    }
+    public void setDesc(String desc){
+        this.description = desc;
+    }
+    public void setRank(int r){
+        this.minRank=r;
+    }
+    public void setBonus(int b){
+        this.rehearseBonus = b;
+    }
+    public void setActorID(int i){
+        this.actor = i;
+    }
 }
